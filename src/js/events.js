0bcd2c450af67dddae121b18cb59f3af20b1d1c8
nsingla <nitin.nyu@gmail.com>
2021-10-06 23:21:05 -0400

    display song title

--- src/js/events.js
+++ src/js/events.js
@@ -2,19 +2,21 @@ const path = window.require('path');
 const fs = window.require('fs');
 var files = fs.readdirSync(path.join(__dirname, 'assets', 'music_list'));
 var audio = new Audio();
-audio.volume = 0.8;
+audio.volume = 0.6;
 
 
 function playMusic(isToggle){
-    if (isToggle.checked){
-        if (audio.src.length == 0){
-            var random_file = files[Math.floor(Math.random() * files.length)];
-            const file = path.join(__dirname, 'assets', 'music_list', random_file);
-            audio = new Audio(file);
-        }
-        audio.play();
+    let label = document.getElementById('musicTitle_id');
+    if (isToggle){
+        var random_file = files[Math.floor(Math.random() * files.length)];
+        const file = path.join(__dirname, 'assets', 'music_list', random_file);
+        audio = new Audio(file);
+        audio.addEventListener('ended', function() {playMusic(true);});
+        label.innerHTML = random_file;
+        audio.play();        
     }
     else{
+        label.innerHTML = '';
         audio.pause();
     }
 }
